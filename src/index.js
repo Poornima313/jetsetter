8619b3240c1c758bdcfbc4a3b600eaa76fc17ed3
Poornima313 <poornima31p@gmail.com>
2020-09-18 12:38:37 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import Application from "./components/Application";
+
+import "./index.css";
+
+render(<Application />, document.getElementById("root"));
